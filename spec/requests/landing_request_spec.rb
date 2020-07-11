84acc41e1e8a6afd3ee000467f9bc22ffb1e6f3b
Travis Spangle <Travis.Spangle@GettyImages.com>
2020-07-11 11:49:57 -0700

    added tracking and spec

--- spec/requests/landing_request_spec.rb
+++ spec/requests/landing_request_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Landings", type: :request do
-
   describe "GET /home" do
-    it "returns http success" do
-      get "/landing/home"
+    let(:file_path) { '/Users/tspangle/dev/dump/pagliacci/tmp/tracker.txt' }
+
+    before do
+      File.delete(file_path) if File.exist?(file_path)
+    end
+
+    it "tracks users hitting landing" do
+      get "/"
+
       expect(response).to have_http_status(:success)
+      expect(File.exist?(file_path)).to be_truthy
     end
   end
 
